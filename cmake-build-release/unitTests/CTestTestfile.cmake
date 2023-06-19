4bb49932d88aa232f92f302f7089bfd2ece8c88a
Iris Marjollet <118899617+Iris-Marjollet@users.noreply.github.com>
2023-06-19 18:53:19 +0200

    Delete cmake-build-release directory

--- cmake-build-release/unitTests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sarahzakon/Desktop/TSP/2A/C++/project/jeu_monsieur_madame/unitTests
-# Build directory: /Users/sarahzakon/Desktop/TSP/2A/C++/project/jeu_monsieur_madame/cmake-build-release/unitTests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(that-test-I-made "/Users/sarahzakon/Desktop/TSP/2A/C++/project/jeu_monsieur_madame/cmake-build-release/unitTests/unitTests")
-set_tests_properties(that-test-I-made PROPERTIES  _BACKTRACE_TRIPLES "/Users/sarahzakon/Desktop/TSP/2A/C++/project/jeu_monsieur_madame/unitTests/CMakeLists.txt;42;add_test;/Users/sarahzakon/Desktop/TSP/2A/C++/project/jeu_monsieur_madame/unitTests/CMakeLists.txt;0;")
-subdirs("googletest-build")
